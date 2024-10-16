1b360e92d11dbac135351178dd7ddc4ef6a98b87
Marcel Półkośnik <marcel.polkosnik@student.uw.edu.pl>
2024-10-17 00:17:59 +0200

    event order executed

---
+++ src/event/events/event_order_executed.py
@@ -0,0 +1,23 @@
+from src.event.event import Event
+from src.event.event_types import EventType
+
+class EventOrderExecuted(Event):
+
+    type = EventType.ORDER_EXECUTED
+    executable = False
+
+    def __init__(self, timestamp: int, trigger_event_id: int, ticker: str, order_id: int, agent_id: int, id: int = None):
+        
+        super().__init__(timestamp = timestamp, id = id, trigger_event_id = trigger_event_id, ticker = ticker, order_id = order_id, agent_id = agent_id)
+        self.create_message()
+
+    def process(self):
+        pass
+
+    @classmethod
+    def csv_attributes(cls):
+        return super().csv_attributes() + ["ticker", "order_id", "agent_id"]
+    
+    def create_message(self):
+        return super().create_message() + f"Order {self.order_id} {self.ticker} (issuer: {self.agent_id} ) executed."
+    
